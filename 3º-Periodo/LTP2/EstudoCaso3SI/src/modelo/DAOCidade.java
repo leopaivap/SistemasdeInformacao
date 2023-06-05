4f30cd843a25076a32f4e88cd96c2929978e79e0
Leo <paivaleo7@gmail.com>
2023-06-05 08:34:59 -0300

    Conexao - com bug

--- 3º-Periodo/LTP2/EstudoCaso3SI/src/modelo/DAOCidade.java
+++ 3º-Periodo/LTP2/EstudoCaso3SI/src/modelo/DAOCidade.java
@@ -1,11 +1,31 @@
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
-public class DAOCidade { // Data Access Object
+public class DAOCidade { // Data Access Object 
     
     public List<Cidade> getLista(){
-        return Dados.listaCidade;
+        String sql = "select * from cidade";
+        List<Cidade> listaCidade = new ArrayList<>();
+        try{
+            PreparedStatement pst  = Conexao.getPreparedStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                Cidade objCidade = new Cidade();
+                objCidade.setCodigoCidade(rs.getInt("codigo"));
+                objCidade.setNomeCidade(rs.getString("nome"));
+                objCidade.setUfCidade(rs.getString("uf"));
+                listaCidade.add(objCidade);
+            }
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Erro de SQL: " + ex.getMessage());
+        }
+        return listaCidade;
     }
     
     public boolean salvar(Cidade obj){
