f6d617329317e79c666e7c8b3af01aa83e4d4603
Leo <paivaleo7@gmail.com>
2023-10-02 08:25:57 -0300

    sale update

--- 4º Período/LTPIII/Eclipse_Workspace/NotaVenda/src/model/Sale.java
+++ 4º Período/LTPIII/Eclipse_Workspace/NotaVenda/src/model/Sale.java
@@ -1,15 +1,19 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class Sale {
 
 	private List<SaleItem> cart = new ArrayList<>();
 	private double totalPriceCart;
+	private Date date;
 
 	public Sale() {
-
+		this.date = Calendar.getInstance().getTime();
+		this.cart = new ArrayList<>();
 	}
 
 	public void addCart(Product prod, int amount) throws Exception {
@@ -18,14 +22,24 @@ public class Sale {
 		totalPriceCart += item.getTotalPriceItem();
 	}
 
+	public Date getDate() {
+		return date;
+	}
+
 	public void printSale() {
-		for (SaleItem saleItem : cart) {
-			System.out.println("Produto: " + saleItem.getSaleProduct().getName());
-			System.out.println("Quantidade: " + saleItem.getAmount());
-			System.out.println("Valor Unitário: R$" + saleItem.getSaleProduct().getCost());
-			System.out.println("Valor Total: R$" + saleItem.getTotalPriceItem());
-			System.out.println("----------------------------------");
+		if (cart != null) {
+			System.out.println(String.format("Venda - #1234 - %s\n", this.getDate().toLocaleString()));
+			System.out.println("\tProduto\t\tQtd\tUnd\tTotal\n");
+			for (SaleItem saleItem : cart) {
+				String productName = saleItem.getSaleProduct().getName();
+				if (productName.length() > 6)
+					productName = saleItem.getSaleProduct().getName().substring(0, 6);
+
+				System.out.println("\t" + productName + "\t\t" + saleItem.getAmount() + "\t"
+						+ saleItem.getSaleProduct().getCost() + "\t" + saleItem.getTotalPriceItem());
+				System.out.println("--------------------------------------------------------");
+			}
+			System.out.println("Total Carrinho: R$" + this.totalPriceCart);
 		}
-		System.out.println("Valor Total Carrinho: R$" + this.totalPriceCart);
 	}
 }
