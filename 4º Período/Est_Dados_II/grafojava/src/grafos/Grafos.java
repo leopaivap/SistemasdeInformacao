0a8b48744d4131329d31d94ef511b4660be775d6
Leonardo Paiva Pinto <02210412650@CAMPUS.MACHADO.MCH>
2023-11-22 09:14:59 -0300

    alcance grafo

--- 4º Período/Est_Dados_II/grafojava/src/grafos/Grafos.java
+++ 4º Período/Est_Dados_II/grafojava/src/grafos/Grafos.java
@@ -1,20 +1,73 @@
 package grafos;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Grafos {
+
+    static int menuMetodos() {
+        Scanner scanner = new Scanner(System.in);
+        int op;
+        System.out.println("1-Adicionar Vertice");
+        System.out.println("2-Adicionar Aresta");
+        System.out.println("3-Imprime Grafo");
+        System.out.println("4-Alcance Grafo");
+        System.out.println("5-Busca em Profundidade [DFS]");
+        System.out.println("6-Busca em Largura [BFS]");
+        System.out.println("0-Sair");
+        op = scanner.nextInt();
+        return op;
+    }
 
     public static void main(String[] args) {
         Graph grafo = new Graph();
-        grafo.adicionarVertice(1);
-        grafo.adicionarVertice(2);
-        grafo.adicionarVertice(3);
-        grafo.adicionarVertice(4);
+        Scanner scanner = new Scanner(System.in);
+        int op = 0;
+        grafo.carregarGrafo();
 
-        grafo.adicionarAresta(1, 2, 3);
-        grafo.adicionarAresta(1, 3, 5);
-        grafo.adicionarAresta(2, 4, 2);
-        grafo.adicionarAresta(3, 4, 1);
+        do {
+            op = menuMetodos();
+            switch (op) {
+                case 1:
+                    System.out.println("Vertice:");
+                    int valor = scanner.nextInt();
+                    grafo.adicionarVertice(valor);
+                    break;
 
-        grafo.imprimirGrafo();
+                case 2:
+                    System.out.println("Adjacencias:");
+                    int origem = scanner.nextInt();
+                    int destino = scanner.nextInt();
+                    System.out.println("Peso:");
+                    int peso = scanner.nextInt();
+                    grafo.adicionarAresta(origem, destino, peso);
+                    grafo.imprimirGrafo();
+                    break;
+
+                case 3:
+                    grafo.imprimirGrafo();
+                    break;
+
+                case 4:
+                    System.out.println("Vertices");
+                    System.out.println("Origem:");
+                    origem = scanner.nextInt();
+                    System.out.println("Destino:");
+                    destino = scanner.nextInt();
+
+                    if (grafo.alcance(origem, destino)) 
+                        System.out.println("Existe Caminho!");
+                    else 
+                        System.out.println("Nao Existe Caminho!");
+                    
+                    break;
+
+                case 0:
+                    System.out.println("Saindo");
+                    grafo.salvarGrafo();
+                    break;
+            }
+        } while (op != 0);
 
     }
 
--- 4º Período/Est_Dados_II/grafojava/src/grafos/Graph.java
+++ 4º Período/Est_Dados_II/grafojava/src/grafos/Graph.java
@@ -1,33 +1,64 @@
 package grafos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Graph {
 
-    private Map<Integer, LinkedList<Aresta>> listaAdjacencia;
+    private Map<Integer, LinkedList<Aresta>> meuGrafo;
 
     public Graph() {
-        this.listaAdjacencia = new HashMap<>();
+        this.meuGrafo = new HashMap<>();
     }
 
     // Método para adicionar um vértice ao grafo
     public void adicionarVertice(int vertice) {
-        if (!listaAdjacencia.containsKey(vertice)) {
-            listaAdjacencia.put(vertice, new LinkedList<>());
+        if (!meuGrafo.containsKey(vertice)) {
+            meuGrafo.put(vertice, new LinkedList<>());
         }
     }
 
     public void adicionarAresta(int origem, int destino, int peso) {
-        if (!listaAdjacencia.containsKey(origem) || !listaAdjacencia.containsKey(destino)) {
+        if (!meuGrafo.containsKey(origem) || !meuGrafo.containsKey(destino)) {
             throw new IllegalArgumentException("Os vértices de origem e destino devem existir no grafo.");
         }
 
-        listaAdjacencia.get(origem).add(new Aresta(destino, peso));
-        listaAdjacencia.get(destino).add(new Aresta(origem, peso)); //grafo nao direcionado
+        meuGrafo.get(origem).add(new Aresta(destino, peso));
+        //listaAdjacencia.get(destino).add(new Aresta(origem, peso));
+
+    }
+
+    public boolean alcance(int origem, int destino) {
+        ArrayList visitados = new ArrayList();
+        return existeCaminho(origem, destino, visitados);
+    }
+
+    private boolean existeCaminho(int verticeAtual, int destino, ArrayList visitados) {
+        if (verticeAtual == destino) {
+            System.out.println("Caminho: " + visitados);
+            return true;
+        }
+
+        visitados.add(verticeAtual);
+        LinkedList<Aresta> adjacencias = meuGrafo.get(verticeAtual);
+
+        if (adjacencias != null) {
+            for (Aresta vizinho : adjacencias) {
+                if (!visitados.contains(vizinho.vertice) && existeCaminho(vizinho.vertice, destino, visitados)) {
+                    return true;
+                }
+            }
+        }
+        System.out.println("Caminho: " + visitados);
+        return false;
     }
 
     public void imprimirGrafo() {
-        for (Map.Entry<Integer, LinkedList<Aresta>> entry : listaAdjacencia.entrySet()) {
+        for (Map.Entry<Integer, LinkedList<Aresta>> entry : meuGrafo.entrySet()) {
             int vertice = entry.getKey();
             LinkedList<Aresta> vizinhos = entry.getValue();
             System.out.print(vertice + " -> ");
@@ -35,6 +66,37 @@ public class Graph {
                 System.out.print(vizinho.vertice + "[" + vizinho.peso + "]" + " ");
             }
             System.out.println();
+        }
+    }
+
+    public void salvarGrafo() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("grafo.txt"))) {
+            for (Map.Entry<Integer, LinkedList<Aresta>> entry : meuGrafo.entrySet()) {
+                int vertice = entry.getKey();
+                LinkedList<Aresta> vizinhos = entry.getValue();
+                for (Aresta vizinho : vizinhos) {
+                    writer.println(vertice + " " + vizinho.vertice + " " + vizinho.peso);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para carregar os dados do grafo de um arquivo texto
+    public void carregarGrafo() {
+        try (Scanner scanner = new Scanner(new File("grafo.txt"))) {
+            while (scanner.hasNext()) {
+                int origem = scanner.nextInt();
+                int destino = scanner.nextInt();
+                int peso = scanner.nextInt();
+
+                adicionarVertice(origem);
+                adicionarVertice(destino);
+                adicionarAresta(origem, destino, peso);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
