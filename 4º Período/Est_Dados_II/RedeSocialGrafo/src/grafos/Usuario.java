09c200c1fa48d87b525187a6b21cd1f0a82a3adb
Leo <paivaleo7@gmail.com>
2023-12-04 15:13:14 -0300

    RedeSocial Grafo - Atv VI - Est.Dad II

---
+++ 4º Período/Est_Dados_II/RedeSocialGrafo/src/grafos/Usuario.java
@@ -0,0 +1,13 @@
+package grafos;
+
+public class Usuario {
+
+    int vertice, distancia;
+    String nome, interesses;
+
+    public Usuario(int vertice, int distancia) {
+        this.vertice = vertice;
+        this.distancia = distancia;
+    }
+
+}
