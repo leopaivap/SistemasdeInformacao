803afac2ec9285771ab6dda1eef8edba80008def
Leonardo Paiva Pinto <02210412650@CAMPUS.MACHADO.MCH>
2024-04-12 08:18:03 -0300

    parte 2 web controle financeiro

--- 5º Período/WebII/web-controle-financeiro/controle.js
+++ 5º Período/WebII/web-controle-financeiro/controle.js
@@ -2,8 +2,9 @@ const form = document.getElementById('form');
 const descInput = document.getElementById('descricao');
 const valorInput = document.querySelector('#montante');
 const balancoH1 = document.getElementById('balanco');
-
-
+const receita = document.getElementById('din-positivo');
+const despesa = document.getElementById('din-negativo');
+const transacaoUL = document.getElementById('transacoes');
 
 form.addEventListener("submit", event => {
     event.preventDefault(); // Não submete formulário
@@ -23,14 +24,15 @@ form.addEventListener("submit", event => {
         return;
     }
 
-
     const transacao = {
         id: parseInt(Math.random() * 10000),
         desc: descTransacao,
-        value: valorTransacao
+        value: parseFloat(valorTransacao)
     }
 
     somaAoSaldo(transacao);
+    somaReceitaDespesa(transacao);
+    addTransacaoDOM(transacao);
 
     descInput.value = '';
     valorInput.value = '';
@@ -43,7 +45,33 @@ function somaAoSaldo(transacao) {
 
     let valorBalanco = balancoH1.innerHTML.trim();
     valorBalanco = valorBalanco.replace("R$", "");
-    valorBalanco = parseFloat(valorBalanco);
 
-    alert(typeof valorBalanco + ": "+valorBalanco);
+    valorBalanco = parseFloat(valorBalanco);
+    valorBalanco += transacao.value;
+
+    balancoH1.innerHTML = `R$${valorBalanco.toFixed(2)}`;
+
+    //alert(typeof valorBalanco + ": " + valorBalanco);
+}
+
+function somaReceitaDespesa(transacao) {
+    const elemento = transacao.value > 0 ? receita : despesa;
+    const substituir = transacao.value > 0 ? "+ R$" : "- R$";
+
+    let valor = elemento.innerHTML.replace(substituir, "");
+    valor = parseFloat(valor);
+    valor += Math.abs(transacao.value);
+
+    elemento.innerHTML = `${substituir}${valor.toFixed(2)}`;
+}
+
+function addTransacaoDOM(transacao) {
+    const cssClass = transacao.value > 0 ? 'positivo' : 'negativo';
+    const currency = transacao.valur > 0 ? '$R$' : '-R$';
+    const liElementStr = `${transacao.desc} <span>${currency}${Math.abs(transacao.value)}</span><button class="delete-btn">X</button>`;
+
+    const liElement = document.createElement('li');
+    liElement.innerHTML = liElementStr;
+    liElement.classList.add(cssClass);
+    transacaoUL.appendChild(liElement);
 }
