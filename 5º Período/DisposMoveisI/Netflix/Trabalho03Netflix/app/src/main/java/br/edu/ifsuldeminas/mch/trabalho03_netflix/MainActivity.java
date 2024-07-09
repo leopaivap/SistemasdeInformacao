ffc126680edcb4b9f2da6b6e2e7971b127277bcd
Leonardo Paiva Pinto <02210412650@CAMPUS.MACHADO.MCH>
2024-07-09 09:06:04 -0300

    Trabalho 03 - Layout Netflix

---
+++ 5º Período/DisposMoveisI/Netflix/Trabalho03Netflix/app/src/main/java/br/edu/ifsuldeminas/mch/trabalho03_netflix/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsuldeminas.mch.trabalho03_netflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
